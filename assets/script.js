b891e799d4c41958d94b4962916c86e17e460e7f
Fatalis <connman2400@gmail.com>
2022-10-31 19:52:26 -0500

    // got the api's to be fully  functional

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,111 @@
-var titleEl = document.getElementById("");
-var descriptionEl = document.getElementById("");
-var posterEl = document.getElementById("");
-var releaseEL = document.getElementById("");
-var voteEl = document.getElementById("");
-var searchEL = document.getElementById("search-input");
-var searchBtnEL = document.getElementById("search-btn");
+//var titleEl = document.getElementById('title');
+//var descriptionEl = document.getElementById('description');
+//var posterEl = document.getElementById('poster');
+//var releaseEL = document.getElementById('release');
+//var voteEl = document.getElementById('vote');
+var searchEL = document.getElementById('search-input')
+var searchResults = document.getElementById("results");
+var searchBtnEL = document.getElementById('search-btn');
 var apiKey ="70322234d406f0f338990f054502b826";
-stateName = "Jack+Reacher";
-function movieSearch(){
-let querlyUrl = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey +"&query=" + stateName;
+stateName = "Jack Reacher";
+var globalTitle = "";
 
+function formSubmitHandler(event){
+    event.preventDefault();
+    var movie = searchEL.value.trim();
+    if (movie){
+        movieSearch(movie)
+        searchEL.value = "";
+    } 
+}
+function movieSearch(movies){
+ let querlyUrl = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey +"&query=" + movies;
+// fetches api and converts its content in to data
 fetch(querlyUrl).then(function (response){
-    response.json().then(function(data){
+    response.json().then(async function(data){
+        var dataWithImg = data
+        for (i = 0; i < data.results.length; i++){
+           var poster = await getPoster(data.results[i].original_title);
+           console.log("Poster",poster); 
+           dataWithImg.results[i].posterUrl = poster; 
+        }
+
+
+        console.log("hit"); 
+
+
         //var search = data.results[0];
-        //console.log(search);
-        var title = data.results[0].original_title;
-        var description = data.results[0].overview;
-        var poster = data.results[0].poster_path;
-        var release = data.results[0].release_date;
-        var vote = data.results[0].vote_average; 
-        //console.log(vote);
-        titleEl.innerHTML = title;
-        voteEl.innerHTML = vote + " out of 10 Stars!"
-        posterEl.setAttribute("src",poster);
-        releaseEL.innerHTML = release;
-        descriptionEl.innerHTML = description;
+        console.log(data);
+        searchResults.innerHTML="";
+        
+        //grabs relevant api data and asigns it to a ve
+        for (i = 0; i < data.results.length; i++){
+        
+        var title = data.results[i].original_title;
+        globalTitle = title;
+        var description = data.results[i].overview;
+        var release = data.results[i].release_date;
+        var vote = data.results[i].vote_average;
+        
+        
+        var titleAppend = document.createElement("h2");
+        titleAppend.innerHTML = title;
+
+        searchResults.appendChild(titleAppend);
+
+        
+        console.log("URL",dataWithImg.results[i].posterUrl);
+        var posterAppend = document.createElement("img");
+        posterAppend.setAttribute("src",dataWithImg.results[i].posterUrl);
+        searchResults.appendChild(posterAppend);
+         
+        
+    
+        var voteAppend = document.createElement("h2");
+        voteAppend.innerHTML = vote + " out of 10 Stars!"
+        searchResults.appendChild(voteAppend);
+        
+        var releaseAppend = document.createElement("h3");
+        releaseAppend.innerHTML = release;
+        searchResults.appendChild(releaseAppend);
+        
+        var descriptionAppend = document.createElement("p");
+        descriptionAppend.innerHTML = description;
+        searchResults.appendChild(descriptionAppend);
+        
+        
+              
+        }
+       // titleEl.innerHTML = title;
+        //voteEl.innerHTML = vote + " out of 10 Stars!"
+        //releaseEL.innerHTML = release;
+        //descriptionEl.innerHTML = description;
 
 
     })
 })
 
 }
-searchBtnEL.addEventListener("click", function(){
-    var searchEntry = searchEL.value;
-    movieSearch();
-})
- 
+async function getPoster(title){
+        console.log("title",title)
+        var posterKey = "2af1bbf8";
+        let posterUrl = "http://www.omdbapi.com/?apikey=" + posterKey + "&t=" + title;
+   var response = await fetch(posterUrl)
+   var parsedResponse = await response.json()
+            
+          // console.log(data2);
+           var poster = parsedResponse.Poster;
+            // console.log("poster",poster); 
+           //var posterAppend = document.createElement("img");
+        //    posterAppend.setAttribute("src",poster);
+        //   titleAppend.appendChild(posterAppend);
+         // console.log(posterLink); 
+          
+        return poster; 
+        }
+       
+
+
+searchBtnEL.addEventListener("click",formSubmitHandler)
+
+ console.log(searchResults);
