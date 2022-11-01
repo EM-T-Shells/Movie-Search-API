a74c494eacb0adea93a4fff86bb008fb1769556c
Gokhan Emre Taskale <taskaleg@gmail.com>
2022-11-01 17:31:06 -0500

    app styled, clean appearance, functionality is flawless, and all criterias are satisfied. ready for push-merge-deployment. great job everyone. Project 1 accomplished.

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,15 +4,12 @@ var searchBtnEL = document.getElementById('search-btn');
 var apiKey ="70322234d406f0f338990f054502b826";
 var searchHistoryEl = document.getElementById('search-history');
 var searchCLear = document.getElementById('search-clear');
-//stateName = "Jack Reacher";
 var globalTitle = "";
 let searchHistory = JSON.parse(localStorage.getItem("movie")) || [];
 
 function formSubmitHandler(event){
     event.preventDefault();
     var movie = searchEL.value.trim();
-    //console.log("hello",searchHistory);
-
     searchHistory.push(searchEL.value.trim());
     localStorage.setItem("movie",JSON.stringify(searchHistory));
 
@@ -25,20 +22,15 @@ function formSubmitHandler(event){
 
 function movieSearch(movies){
  let querlyUrl = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey +"&query=" + movies;
+
 // fetches api and converts its content in to data
 fetch(querlyUrl).then(function (response){
     response.json().then(async function(data){
         var dataWithImg = data
         for (i = 0; i < data.results.length; i++){
            var poster = await getPoster(data.results[i].original_title);
-           console.log("Poster",poster);
            dataWithImg.results[i].posterUrl = poster;
         }
-        //console.log("hit");
-
-
-        //var search = data.results[0];
-        //console.log(data);
         searchResults.innerHTML = " ";
 
         //grabs relevant api data and asigns it to a ve
@@ -55,8 +47,7 @@ fetch(querlyUrl).then(function (response){
         titleAppend.innerHTML = title;
         searchResults.appendChild(titleAppend);
 
-
-        console.log("URL",dataWithImg.results[i].posterUrl);
+       // console.log("URL",dataWithImg.results[i].posterUrl);
         var posterAppend = document.createElement("img");
         posterAppend.setAttribute("src",dataWithImg.results[i].posterUrl);
         searchResults.appendChild(posterAppend);
@@ -74,37 +65,20 @@ fetch(querlyUrl).then(function (response){
         var descriptionAppend = document.createElement("p");
         descriptionAppend.innerHTML = description;
         searchResults.appendChild(descriptionAppend);
-
-
-
         }
-       // titleEl.innerHTML = title;
-        //voteEl.innerHTML = vote + " out of 10 Stars!"
-        //releaseEL.innerHTML = release;
-        //descriptionEl.innerHTML = description;
-
-
     })
-})
-
+   })
 }
+
 async function getPoster(title){
-        console.log("title",title)
-        var posterKey = "2af1bbf8";
-        let posterUrl = "http://www.omdbapi.com/?apikey=" + posterKey + "&t=" + title;
+   var posterKey = "2af1bbf8";
+   let posterUrl = "http://www.omdbapi.com/?apikey=" + posterKey + "&t=" + title;
    var response = await fetch(posterUrl)
    var parsedResponse = await response.json()
+   var poster = parsedResponse.Poster;
 
-          // console.log(data2);
-           var poster = parsedResponse.Poster;
-            // console.log("poster",poster);
-           //var posterAppend = document.createElement("img");
-        //    posterAppend.setAttribute("src",poster);
-        //   titleAppend.appendChild(posterAppend);
-         // console.log(posterLink);
-
-        return poster;
-        }
+   return poster;
+}
 
 function getSearchHistory(){
     searchHistoryEl.innerHTML = "";
@@ -112,12 +86,10 @@ function getSearchHistory(){
     searchHistory = JSON.parse(localStorage.getItem("movie"));
     for (i = 0; i < searchHistory.length; i++){
         var appendHistory = document.createElement("li")
-
         appendHistory.innerHTML = searchHistory[i];
         searchHistoryEl.appendChild(appendHistory);
-        //console.log(appendHistory);
-    }
-}
+      }
+   }
 }
 
 searchBtnEL.addEventListener("click",formSubmitHandler)
@@ -130,6 +102,3 @@ function formCLear(){
 if (searchHistory !=""){
 getSearchHistory();
 }
- /*console.log(searchResults);
- console.log(searchHistoryEl);
- console.log(searchHistory);*/
